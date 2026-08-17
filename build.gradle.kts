ff6a9b6a5f785170b0ddb416fc883e029a9c1457
Mahmoud Heydari <mahmoud.heydari.work@outlook.com>
2026-08-17 03:53:13 +0330

    EP-init-001 initial multi-module gradle project structure add build.gradle.kts files in Ecommerce-platfor(EP),common-core,identity-service,product-service,cart-service

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+}
+
+allprojects {
+    group = "com.naderaria.ecommerce"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        maven( url = uri("https://maven.myket.ir"))
+        maven( url = uri("https://en-mirror.ir"))
+        maven( url = uri("https://maven.aliyun.com/repository/public/"))
+        maven( url = uri("https://maven.aliyun.com/repository/gradle-plugin"))
+
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(25))
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    dependencies {
+        // Lombok
+        compileOnly("org.projectlombok:lombok:1.18.46")
+        annotationProcessor("org.projectlombok:lombok:1.18.46")
+
+        // MapStruct
+        implementation("org.mapstruct:mapstruct:1.6.3")
+        annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
+        // Test
+        testImplementation("org.springframework.boot:spring-boot-starter-test:4.1.0")
+    }
+}
---
+++ common/common-core/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api("org.springframework.boot:spring-boot-starter-validation:4.1.0")
+    api("com.fasterxml.jackson.core:jackson-annotations:2.18.3")
+
+    compileOnly("org.projectlombok:lombok:1.18.46")
+    annotationProcessor("org.projectlombo:lombok:1.18.46")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:4.1.0") {}
+}
